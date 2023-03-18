973a09a85bb5594d319cf135db9fd58ce8180d84
MonkeyAndEgg <412793874@qq.com>
2023-03-18 15:48:39 -0700

    added mutation to signup component

--- client/src/pages/Signup/Signup.tsx
+++ client/src/pages/Signup/Signup.tsx
@@ -1,13 +1,51 @@
-import { Button, Flex, FormControl, FormLabel, Heading, Input, VStack } from "@chakra-ui/react";
-import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Button, Flex, FormControl, FormLabel, Heading, Input, Text, VStack } from "@chakra-ui/react";
+import gql from "graphql-tag";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import useForm from "../../hooks/useForm";
+
+const REGISTER_USER = gql`
+  mutation Mutation(
+    $registerInput: RegisterInput
+  ) {
+    registerUser(
+      registerInput: $registerInput
+    ) {
+      email
+      username
+      token
+    }
+  }
+`;
 
 export default function Signup() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const onSignup = () => {
-
+  const [error, setError] = useState('');
+  const authContext = useContext(AuthContext);
+  const navigate = useNavigate();
+  const signupCallback = () => {
+    console.log('calling back');
+    registerUser();
   };
+
+  const { onChange, onSubmit, values } = useForm(signupCallback, {
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  
+  const [registerUser, { loading }] = useMutation(REGISTER_USER, {
+    update(proxy, { data: { registerUser: userData } }) {
+      authContext.login(userData);
+      navigate('/');
+    },
+    onError(err) {
+      setError(err.message);
+    },
+    variables: { registerInput: values }
+  });
 
   return (
     <Flex w='100%' justifyContent={'center'} alignItems={'center'}>
@@ -15,14 +53,23 @@ export default function Signup() {
         <Heading>Sign Up</Heading>
         <FormControl>
           <FormLabel>Username</FormLabel>
-          <Input placeholder='username' onChange={(event) => setUsername(event.target.value)} />
+          <Input placeholder='username' name='username' onChange={onChange} />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input placeholder='email' name='email' onChange={onChange} />
         </FormControl>
         <FormControl>
           <FormLabel>Password</FormLabel>
-          <Input placeholder='password' onChange={(event) => setPassword(event.target.value)} />
+          <Input placeholder='password' name='password' onChange={onChange} />
         </FormControl>
+        <FormControl>
+          <FormLabel>Confirm Password</FormLabel>
+          <Input placeholder='confirm password' name='confirmPassword' onChange={onChange} />
+        </FormControl>
+        <Text>{error}</Text>
         <Flex w='100%' justifyContent={'end'}>
-          <Button colorScheme='teal' onClick={onSignup}>Sign Up</Button>
+          <Button colorScheme='teal' onClick={()=>{console.log(values);onSubmit();}}>Register</Button>
         </Flex>
       </VStack>
     </Flex>
