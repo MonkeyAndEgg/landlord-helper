a1ef81079cc5d2527bbf09cd0e4f59ff046cc348
MonkeyAndEgg <412793874@qq.com>
2023-03-19 14:58:20 -0700

    added links on side bar and updated user component

--- client/src/components/SideBar/SideBarContent/SideBarContent.tsx
+++ client/src/components/SideBar/SideBarContent/SideBarContent.tsx
@@ -1,36 +1,61 @@
 import { Box, Flex, Link, VStack } from "@chakra-ui/react";
+import { useContext } from "react";
+import { AuthContext } from "../../../context/authContext";
 import User from "../../User/User";
 
 export default function SideBarContent() {
-
+  const authContext = useContext(AuthContext);
 
   return (
     <Box>
-      <User />
+      <User user={authContext.user} />
 
       <VStack
         mt={20}
         spacing={10}
         align='stretch'
       >
-        
-        <Link href='/balance'>
-          <Flex h='40px' cursor='pointer' justifyContent='center'>
-            Balance
-          </Flex>
-        </Link>
+        { authContext.user ? (
+          <>
+            <Link href='/balance'>
+              <Flex h='40px' cursor='pointer' justifyContent='center'>
+                Balance
+              </Flex>
+            </Link>
 
-        <Link href='/analysis'>
-          <Flex h='40px' cursor='pointer' justifyContent='center'>
-            Analysis
-          </Flex>
-        </Link>
+            <Link href='/analysis'>
+              <Flex h='40px' cursor='pointer' justifyContent='center'>
+                Analysis
+              </Flex>
+            </Link>
 
-        <Link href='/routine'>
-          <Flex h='40px' cursor='pointer' justifyContent='center'>
-            Routine
-          </Flex>
-        </Link>
+            <Link href='/routine'>
+              <Flex h='40px' cursor='pointer' justifyContent='center'>
+                Routine
+              </Flex>
+            </Link>
+
+            <Link href='/login'>
+              <Flex h='40px' cursor='pointer' justifyContent='center'>
+                Logout
+              </Flex>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link href='/login'>
+              <Flex h='40px' cursor='pointer' justifyContent='center'>
+                Login
+              </Flex>
+            </Link>
+
+            <Link href='/signup'>
+              <Flex h='40px' cursor='pointer' justifyContent='center'>
+                Signup
+              </Flex>
+            </Link>
+          </>
+        ) }
       </VStack>
     </Box>
   );
--- client/src/components/User/User.tsx
+++ client/src/components/User/User.tsx
@@ -1,17 +1,22 @@
 import { Avatar, Badge, Box, Flex, Text } from "@chakra-ui/react";
 
-export default function User() {
+interface IUser {
+  user: any
+}
+
+export default function User({ user }: IUser) {
+
   return (
-    <Flex justifyContent='center' py={5}>
+    user && <Flex justifyContent='center' py={5}>
       <Avatar src='https://bit.ly/sage-adebayo' />
       <Box ml='3'>
         <Text fontWeight='bold'>
-          Segun Adebayo
+          {user.username}
           <Badge ml='1' colorScheme='green'>
             Online
           </Badge>
         </Text>
-        <Text fontSize='sm'>UI Engineer</Text>
+        <Text fontSize='sm'>{user.email}</Text>
       </Box>
     </Flex>
   );
