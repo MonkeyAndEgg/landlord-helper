92bf98f8a53f6f6c5c6656e333e307627bcffb2d
MonkeyAndEgg <412793874@qq.com>
2023-04-22 16:21:54 -0700

    added analysis with summary

--- client/src/pages/Analysis/Analysis.tsx
+++ client/src/pages/Analysis/Analysis.tsx
@@ -1,9 +1,94 @@
-import { Flex } from "@chakra-ui/react";
+import { useQuery } from "@apollo/client";
+import { Flex, Text, HStack, Heading, VStack, Select } from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/authContext";
+import { GET_RECORDS } from "../../query/records";
+import { TYPE } from "../../constants/record-type";
 
 export default function Analysis() {
+  const authContext = useContext(AuthContext);
+  const [ yearOptions, setYearOptions ] = useState<number[]>([]);
+  const [ currentYearOption, setCurrentYear ] = useState<number>();
+  const [ fromDate, setFromDate ] = useState<string>();
+  const [ toDate, setToDate ] = useState<string>();
+  const { loading, error, data, refetch } = useQuery(GET_RECORDS, {
+    variables: { getRecordsInput: { userId: authContext && authContext.user ? authContext.user['user_id'] : '', fromDate, toDate } },
+    pollInterval: 500,
+  });
+  const [ summary, setSummary ] = useState({ totalIncome: 0, totalCost: 0, netIncome: 0 });
+
+  useEffect(() => {
+    // initialize year options
+    const options = [];
+    const currentYear = new Date().getUTCFullYear();
+    for (let i = 0; i < 15; i++) {
+      options.push(currentYear - i);
+    }
+    setYearOptions(options);
+    setCurrentYear(currentYear);
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      const records = data.records;
+      const costs = records.filter((record: any) => record.type === TYPE.COST).map((record: any) => +record.amount);
+      const incomes = records.filter((record: any) => record.type === TYPE.INCOME).map((record: any) => +record.amount);
+      const totalIncome = incomes.reduce((acc: number, curr: number) => acc + curr, 0);
+      const totalCost = costs.reduce((acc: number, curr: number) => acc + curr, 0);
+      setSummary({ totalIncome, totalCost, netIncome: totalIncome - totalCost });
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (currentYearOption) {
+      setFromDate(`${currentYearOption-1}-${new Date().getUTCMonth()+1}-${new Date().getUTCDate()}`);
+      setToDate(`${currentYearOption}-${new Date().getUTCMonth()+1}-${new Date().getUTCDate()}`);
+    }
+  }, [currentYearOption]);
+
+  
   return (
-    <Flex>
-      Analysis page
-    </Flex>
+    <VStack w="100%">
+      <Flex w="100%" justifyContent="start">
+        <Select placeholder='Select year' w="200px" value={currentYearOption} onChange={(e) => setCurrentYear(+e.target.value)}>
+          {
+            yearOptions.map(year => <option value={year} key={year}>{year}</option>)
+          }
+        </Select>
+      </Flex>
+      <Flex w="100%" h="100px" p={10} borderWidth='1px' borderRadius='lg' overflow='hidden' justifyContent="center">
+        { error ? (
+            <Flex fontWeight={900}>Failed on display summary</Flex>
+          ) : (
+            <HStack spacing={10} w="100%" justifyContent="space-evenly">
+              <VStack alignItems="start">
+                <Heading as='h5' size='sm'>
+                  Total Income
+                </Heading>
+                <Text>
+                  ${summary.totalIncome}
+                </Text>
+              </VStack>
+              <VStack alignItems="start">
+                <Heading as='h5' size='sm'>
+                  Total Cost
+                </Heading>
+                <Text>
+                  ${summary.totalCost}
+                </Text>
+              </VStack>
+              <VStack alignItems="start">
+                <Heading as='h5' size='sm'>
+                  Net Income
+                </Heading>
+                <Text>
+                  ${summary.netIncome}
+                </Text>
+              </VStack>
+            </HStack>
+          )
+        }
+      </Flex>
+    </VStack>
   );
 }
