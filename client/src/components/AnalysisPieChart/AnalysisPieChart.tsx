3c7b4a929d174bfc779712ed147fb3996dd4a7a6
MonkeyAndEgg <412793874@qq.com>
2023-04-22 22:42:48 -0700

    created pie chart component

---
+++ client/src/components/AnalysisPieChart/AnalysisPieChart.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { PieChart } from "react-minimal-pie-chart";
+
+const LINE_WIDTH_PIE_CHART = 60;
+
+interface AnalysisPieChartProps {
+  data: any
+}
+
+export default function AnalysisPieChart({ data }: AnalysisPieChartProps) {
+  const [ selected, setSelected] = useState<number | undefined>(0);
+  const [ hovered, setHovered ] = useState<number | undefined>(undefined);
+
+  const displayData = data.map((entry: any, i: number) => {
+    if (hovered === i) {
+      return {
+        ...entry,
+        color: 'grey',
+      };
+    }
+    return entry;
+  });
+  
+  return (
+    <PieChart
+      style={{
+        fontFamily:
+          '"Nunito Sans", -apple-system, Helvetica, Arial, sans-serif',
+        fontSize: '8px',
+      }}
+      data={displayData}
+      radius={44}
+      lineWidth={LINE_WIDTH_PIE_CHART}
+      totalValue={45}
+      label={({ dataEntry }) => Math.round(dataEntry.percentage) + '%'}
+      animate
+      segmentsStyle={{ transition: 'stroke .3s', cursor: 'pointer' }}
+      segmentsShift={(index) => (index === selected ? 6 : 1)}
+      labelStyle={{
+        fill: '#fff',
+        opacity: 0.75,
+        pointerEvents: 'none',
+      }}
+      labelPosition={100 - LINE_WIDTH_PIE_CHART / 2}
+      onClick={(_, index) => {
+        setSelected(index === selected ? undefined : index);
+      }}
+      onMouseOver={(_, index) => {
+        setHovered(index);
+      }}
+      onMouseOut={() => {
+        setHovered(undefined);
+      }}
+    />
+  );
+};
