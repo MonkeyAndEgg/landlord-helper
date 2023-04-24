75bafe06789a26fe3262d0df89be72032db9c202
MonkeyAndEgg <412793874@qq.com>
2023-04-23 21:30:03 -0700

    added category constant

---
+++ client/src/constants/category.ts
@@ -0,0 +1,42 @@
+export const CATEGORY = {
+  UTILITY: {
+    label: 'Utility',
+    color: '#F0E68C'
+  },
+  MORTGAGE_PRIN: {
+    label: 'Mortgage Principal',
+    color: '#B22222'
+  },
+  MORTGAGE_INTEREST: {
+    label: 'Mortgage Interest',
+    color: '#FF4500'
+  },
+  MAINTAINENCE: {
+    label: 'Maintainence',
+    color: '#87CEFA'
+  },
+  NETWORK: {
+    label: 'Network',
+    color: '#4B0082'
+  },
+  PROPERTY_TAX: {
+    label: 'Property Tax',
+    color: '#FF69B4'
+  },
+  RENT: {
+    label: 'Rent',
+    color: '#7CFC00'
+  },
+  FERRY_TICKETS: {
+    label: 'Ferry Tickets',
+    color: '#7FFFD4'
+  },
+  INSURANCE: {
+    label: 'Insurance',
+    color: '#00BFFF'
+  },
+  GARDENING: {
+    label: 'Gardening',
+    color: '#ADFF2F'
+  }
+}
